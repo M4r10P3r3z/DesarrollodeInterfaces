bc0c2b43435ba3d78e151807b50c782b2e1a0b2c
Mario <mpm0032@alu.medac.es>
2023-12-06 11:10:08 +0100

    v 1.0 Version Final

--- src/main/java/vista/VentanaTabla.java
+++ src/main/java/vista/VentanaTabla.java
@@ -27,7 +27,7 @@ public class VentanaTabla extends JDialog {
         setTitle("Datos Consultados");
         setContentPane(contentPane);
         pack();
-        setSize(900,300);
+        setSize(900, 300);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
         getRootPane().setDefaultButton(bVerInforme);
@@ -36,28 +36,27 @@ public class VentanaTabla extends JDialog {
     }
 
     private void createUIComponents() {
-        String [] columnas = new String[] {
-            "Numero Expediente", "Fecha de Entrada", "Duración Estimada", "Código del Cliente",
+        String[] columnas = new String[]{
+                "Numero Expediente", "Fecha de Entrada", "Duración Estimada", "Código del Cliente",
                 "Código de la Vivienda", "¿Ya Cobrado?"
         };
-        Object [][] datos = new Object[this.controlador.getAlquileres().size()][6];
+        Object[][] datos = new Object[this.controlador.getAlquileres().size()][6];
         for (int i = 0; i < this.controlador.getAlquileres().size(); i++) {
-            datos [i][0] = this.controlador.getAlquileres().get(i).getNumeroExpediente();
-            datos [i][1] = this.controlador.getAlquileres().get(i).getFechaEntrada();
-            datos [i][2] = this.controlador.getAlquileres().get(i).getDuracionEstimada();
-            datos [i][3] = this.controlador.getAlquileres().get(i).getCliente().getId();
-            datos [i][4] = this.controlador.getAlquileres().get(i).getVivienda().getId();
-            datos [i][5] = this.controlador.getAlquileres().get(i).getEstaCobrado();
+            datos[i][0] = this.controlador.getAlquileres().get(i).getNumeroExpediente();
+            datos[i][1] = this.controlador.getAlquileres().get(i).getFechaEntrada();
+            datos[i][2] = this.controlador.getAlquileres().get(i).getDuracionEstimada();
+            datos[i][3] = this.controlador.getAlquileres().get(i).getCliente().getId();
+            datos[i][4] = this.controlador.getAlquileres().get(i).getVivienda().getId();
+            datos[i][5] = this.controlador.getAlquileres().get(i).getEstaCobrado();
         }
-        DefaultTableModel modelo = new DefaultTableModel(datos,columnas) {
-            private final Class[] claseColumnas = new Class[] {
+        DefaultTableModel modelo = new DefaultTableModel(datos, columnas) {
+            private final Class[] claseColumnas = new Class[]{
                     Long.class, LocalDate.class, Integer.class, Long.class, Long.class, String.class
             };
             @Override
             public boolean isCellEditable(int row, int column) {
                 return false;
             }
-
             @Override
             public Class<?> getColumnClass(int columnIndex) {
                 return claseColumnas[columnIndex];
@@ -74,17 +73,18 @@ public class VentanaTabla extends JDialog {
     private void reporte() {
         try {
             JasperReport informe = (JasperReport) JRLoader.
-                    loadObject(new File("src/main/resources/reportes/Blank_A4.jasper"));
+                    loadObject(new File("src/main/resources/reportes/InformeDetalle.jasper"));
             JRBeanCollectionDataSource datos = new JRBeanCollectionDataSource(this.controlador.getAlquileres());
             JasperPrint jp = JasperFillManager.fillReport(informe, null, datos);
             JasperViewer ventanaInforme = new JasperViewer(jp, false);
-            ventanaInforme.setSize(1200,700);
+            ventanaInforme.setSize(1200, 800);
             ventanaInforme.setLocationRelativeTo(null);
             ventanaInforme.setVisible(true);
             ventanaInforme.setAlwaysOnTop(true);
             this.dispose();
         } catch (JRException e) {
-            JOptionPane.showMessageDialog(null,"Problemas con el informe",
+            System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, "Problemas con el informe",
                     "ERROR", JOptionPane.ERROR_MESSAGE);
         }
     }
